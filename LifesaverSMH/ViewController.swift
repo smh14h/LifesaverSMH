70f9fdd9cf875eb36a83955d9cc8bcd3ec712454
Sean Halstead <smh14h@acu.edu>
2019-02-27 08:14:29 -0600

    Initial Commit

---
+++ LifesaverSMH/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  LifesaverSMH
+//
+//  Created by Sean Halstead on 2/27/19.
+//  Copyright © 2019 Sean Halstead. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
